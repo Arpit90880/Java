1ee58b65cb6332687916552a72dda1d1b87c966d
Arpit <arpitsingal6098@gmail.com>
2021-03-26 12:27:35 +0530

    done

---
+++ q2.java
@@ -0,0 +1,79 @@
+import java.util.*;
+import java.util.Scanner;
+public class Math{
+static int a;
+static int b;
+public Math(int a,int b){
+    this.a=a;
+    this.b=b;
+}
+     public static void main(String []args){
+         Scanner s=new Scanner(System.in);
+         a=s.nextInt();
+         b=s.nextInt();
+        conditionalOperation(a,b);
+     }
+     public static void conditionalOperation(int a,int b){
+         if(a<b){
+             System.out.println(a+b);
+         }
+         else if(a>b && b!=0){
+             System.out.println(a/b);
+         }
+         else if(a>b){
+             System.out.println(a-b);
+         }
+         else if(a==b){
+             System.out.println(a*b);
+         }
+     }
+}
+
+
+//using case
+import java.util.*;
+import java.util.Scanner;
+public class Math{
+static int a;
+static int b;
+public Math(int a,int b){
+    this.a=a;
+    this.b=b;
+}
+     public static void main(String []args){
+         Scanner s=new Scanner(System.in);
+         a=s.nextInt();
+         b=s.nextInt();
+        conditionalOperation(a,b);
+     }
+     public static void conditionalOperation(int a,int b){
+         int level=0;
+         if(a<b){
+             level=1;
+         }
+         else if(a>b && b!=0){
+             level=2;
+         }
+         else if(a>b){
+             level=3;
+         }
+         else if(a==b){
+             level=4;
+         }
+         
+         switch(level){
+         case 1:
+            System.out.println(a+b);
+            break;
+         case 2 :
+            System.out.println(a/b);
+            break;
+         case 3:
+            System.out.println(a-b);
+            break;
+        case 4: 
+            System.out.println(a*b);   
+        
+     }
+}
+}
---
+++ q3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Math1{
+private int a;
+private int b;
+public String get() {
+    return a+" "+b;
+  }
+ public void set(int a1,int b1) {
+    this.a = a1;
+    this.b = b1;
+  }
+     public static void main(String []args){
+        Math1 m=new Math1();
+        m.set(10,20);
+        System.out.println(m.get());
+     }
+}
